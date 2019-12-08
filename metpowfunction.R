fdcba0d9a86b3cadf6a3ea468a0918c922c90e2b
Winther Andreas Kjæreng (awi027) <awi027@rstudio.uit.no>
2019-12-08 01:18:43 +0100

    Added butterworth filter

--- metpowfunction.R
+++ metpowfunction.R
@@ -1,3 +1,14 @@
+library(readxl)
 metpowcalc <- read_excel("metabolic power calculations.xlsx")
 
 speed.df <- metpowcalc[1:2]
+
+speed.df <- speed.df %>%
+  mutate(acceleration = c(NA,diff(speed)/diff(time)),
+         alpha = rad2deg(atan(9.81/acceleration)),
+         g = sqrt(acceleration^2 + 9.81^2),
+         equivilantSlope = tan(0.5 * pi - atan(9.81/acceleration)),
+         equivilantMass = g/9.81,
+         energyCost = ifelse(acceleration == 0, 3.6*1.29, (155.4*equivilantSlope^5 - 30.4*equivilantSlope^4 - 43.3*equivilantSlope^3 + 46.3*equivilantSlope^2 + 19.5*equivilantSlope + 3.6)*equivilantMass*1.29),
+         metabolicPower = energyCost*speed)
+
---
+++ miscCode.R
@@ -0,0 +1,47 @@
+#specific player datasets
+player1 <- df[df$tag_id == 1,]
+
+player1_200_380 <- player1[player1$seconds >200 & player1$seconds < 380,]
+
+ggplot(data = player1,
+       aes(x = seconds, y = speed_km.h, color = speed_km.h)) +
+  geom_line() +
+  scale_colour_gradientn(colours = rainbow(7)) +
+  xlab("\n Time (s)") +
+  ylab(expression(Velocity ~ (m.s^-1))) +
+  scale_x_continuous(limits = c(200, 380), expand = c(0, 0), breaks = seq(200, 380, by = 20)) +
+  scale_y_continuous() +
+  theme_classic() +
+  theme(legend.position = "none") #+ 
+facet_wrap(tag_id ~ .)
+
+pitchBG <- function(lengthPitch = 105, widthPitch = 68, fill = "white", colour = "dimgrey", size = 1) 
+{
+  ggplot() +
+    # perimeter line
+    geom_rect(aes(xmin = 0, xmax = lengthPitch, ymin = 0, ymax = widthPitch), fill = fill, colour = colour, size = 1) +
+    # centre circle
+    geom_circle(aes(x0 = lengthPitch/2, y0 = widthPitch/2, r = 9.15), colour = colour, size = 1) +
+    # kick off spot
+    geom_circle(aes(x0 = lengthPitch/2, y0 = widthPitch/2, r = 0.25), fill = fill, colour = colour, size = 1) +
+    # halfway line
+    geom_segment(aes(x = lengthPitch/2, y = 0, xend = lengthPitch/2, yend = widthPitch), colour = colour, size = 1) + # penalty arcs
+    geom_arc(aes(x0 = 11, y0 = widthPitch/2, r = 9.15, start = 0.65, end = 2.49), colour = colour, size = 1) +
+    geom_arc(aes(x0 = lengthPitch - 11, y0 = widthPitch/2, r = 9.15, start = 3.79, end = 5.63), colour = colour, size = 1) +
+    # penalty areas
+    geom_rect(aes(xmin = 0, xmax = 16.5, ymin = widthPitch/2 - 20.15, ymax = widthPitch/2 + 20.15), fill = NA, colour = colour, size = 1) +
+    geom_rect(aes(xmin = lengthPitch - 16.5, xmax = lengthPitch, ymin = widthPitch/2 - 20.15, ymax = widthPitch/2 + 20.15), fill = NA, colour = colour, size = 1) +
+    # penalty spots
+    geom_circle(aes(x0 = 11, y0 = widthPitch/2, r = 0.25), fill = colour, col = colour, size = 1) +
+    geom_circle(aes(x0 = lengthPitch - 11, y0 = widthPitch/2, r = 0.25), fill = colour, colour = colour, size = 1) +
+    # six yard boxes
+    geom_rect(aes(xmin = 0, xmax = 5.5, ymin = (widthPitch/2) - 9.16, ymax = (widthPitch/2) + 9.16), fill = NA, colour = colour, size = 1) +
+    geom_rect(aes(xmin = lengthPitch - 5.5, xmax = lengthPitch, ymin = (widthPitch/2) - 9.16, ymax = (widthPitch/2) + 9.16), fill = NA, colour = colour, size = 1) +
+    # goals
+    geom_rect(aes(xmin = -2, xmax = 0, ymin = (widthPitch/2) - 3.66, ymax = (widthPitch/2) + 3.66), fill = NA, colour = colour, size = 1) +
+    geom_rect(aes(xmin = lengthPitch, xmax = lengthPitch + 2, ymin = (widthPitch/2) - 3.66, ymax = (widthPitch/2) + 3.66), fill = NA, colour = colour, size = 1) +
+    coord_fixed() +
+    xlab("") +
+    ylab("")
+}
+pitchBG()
